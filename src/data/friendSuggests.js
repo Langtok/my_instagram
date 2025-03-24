571e37c74608ab0d3fa2e18ecb96f0964ebe7caf
Langtock <70757120+Langtok@users.noreply.github.com>
2025-03-24 09:10:02 +0100

    Fix: Suggested friend

--- src/data/friendSuggests.js
+++ src/data/friendSuggests.js
@@ -3,7 +3,7 @@ import { loadUsers, saveUsers } from '../utils/localStorage';
 const defaultUsers = [
   {
     id: "friend1",
-    username: "Mary",
+    username: "Langtock",
     bio: "photo lover",
     profilePicture: "https://static.vecteezy.com/system/resources/thumbnails/010/243/858/small/close-up-shot-of-happy-young-european-woman-has-pleasant-smile-keeps-hands-together-near-chin-wears-casual-white-clothes-isolated-over-grey-background-being-in-high-spirit-enjoys-spare-time-photo.JPG?w=80&h=80",
     likes: 10,
@@ -13,7 +13,7 @@ const defaultUsers = [
   },
   {
     id: "friend2",
-    username: "Musa",
+    username: "Prince",
     bio: "Travel enthusiast",
     profilePicture: "https://lestimes.com/wp-content/uploads/2017/02/Short-People-Change-Big-Worleds-founder-Realeboha-Moeketsi-2-1.jpg?w=80&h=80",
     likes: 5,
@@ -23,7 +23,7 @@ const defaultUsers = [
   },
   {
     id: "friend3",
-    username: "Fahn",
+    username: "Emmanuel",
     bio: "Foodie",
     profilePicture: "https://www.nationalarchivesstore.org/cdn/shop/products/SKU-01058670-T-Shirt-We-The-People-_2_1024x1024.jpg?v=1603769168&w=80&h=80",
     likes: 8,
@@ -33,7 +33,7 @@ const defaultUsers = [
   },
   {
     id: "friend4",
-    username: "Liam",
+    username: "Matthew",
     bio: "Tech geek",
     profilePicture: "https://randomuser.me/api/portraits/men/1.jpg?w=80&h=80",
     likes: 12,
